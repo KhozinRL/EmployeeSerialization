8214b222a6b5ae91d24b2359ab12b5ab3257c2e2
Р.Л. Хозин <s190291019@live.cs.msu.ru>
2021-02-27 16:05:05 +0300

    Add project files.

---
+++ EmployeeSerialization/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeSerialization
+{
+    class Employee
+    {
+        string _firstName;
+        string _lastName;
+        string _patronymic;
+        string _phone;
+        string _address;
+
+        public Employee(string family, string name, string patronymic, string phone, string address) {
+            _firstName = name;
+            _lastName = family;
+            _patronymic = patronymic;
+            _phone = phone;
+            _address = address;
+        }
+
+        string FirstName {
+            get { return _firstName; }
+        }
+
+        string LastName
+        {
+            get { return _lastName; }
+        }
+
+        string Patronymic
+        {
+            get { return _patronymic; }
+        }
+
+        string Phone
+        {
+            get { return _phone; }
+        }
+
+        string Address
+        {
+            get { return _address; }
+        }
+    }
+}
---
+++ EmployeeSerialization/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace EmployeeSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> employeeList = EmployeeDeserializer();
+
+            int regime;
+
+            string name;
+            string family;
+            string patronymic;
+            string phone;
+            string address;
+            
+            do {
+                Console.WriteLine("Выберете режим: 0 - добавление нового сотрудника, 1 - поиск сотрудников, 2 - выход");
+                regime = Console.Read();
+
+
+
+
+            } while (regime != 2);
+        }
+
+        public static void EmployeeSerializer(IEnumerable<Employee> collection) {
+            using (FileStream fs = new FileStream("Employee.json", FileMode.Create))
+            {
+                JsonSerializer.SerializeAsync(fs, collection);
+            }
+        }
+
+        public static IEnumerable<Employee> EmployeeDeserializer() {
+            using (FileStream fs = new FileStream("Employee.json", FileMode.Open)) {
+                return JsonSerializer.DeserializeAsync<IEnumerable<Employee>>(fs).Result;
+            }
+                
+        }
+
+        public 
+
+        public static void addEmployee() { }
+
+    }
+}
